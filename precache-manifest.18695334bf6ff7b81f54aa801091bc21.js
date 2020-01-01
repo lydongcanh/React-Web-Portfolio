a6bc3399ee3ab587e25923f85552ce655d299fc9
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-01-02 00:47:01 +0700

    Updates

---
+++ precache-manifest.18695334bf6ff7b81f54aa801091bc21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d469e0cb47f7a66ab622000a1ec2398e",
+    "url": "/React-Web-Portfolio/index.html"
+  },
+  {
+    "revision": "429dc03a6b31e867ea4a",
+    "url": "/React-Web-Portfolio/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "429dc03a6b31e867ea4a",
+    "url": "/React-Web-Portfolio/static/js/2.ccb9a12f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/React-Web-Portfolio/static/js/2.ccb9a12f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7dc41dd34753d38704c",
+    "url": "/React-Web-Portfolio/static/js/main.25a27477.chunk.js"
+  },
+  {
+    "revision": "4381c9234988aaa1834f",
+    "url": "/React-Web-Portfolio/static/js/runtime-main.4db19abe.js"
+  }
+]);
